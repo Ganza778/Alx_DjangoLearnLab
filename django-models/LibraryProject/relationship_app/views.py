a86b980cd04ea2e0a4c0f459be75c15edb0f41ea
Aristide <gnaristide@gmail.com>
2025-09-06 13:41:02 +0200

    this is the new files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 
 def register(request):
     if request.method == 'POST':
