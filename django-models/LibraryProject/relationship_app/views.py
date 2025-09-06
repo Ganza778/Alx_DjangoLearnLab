88019e422a4fb84b8fca0dea481f0cb286faa058
Aristide <gnaristide@gmail.com>
2025-09-06 13:22:00 +0200

    this is the new files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
-from .models import Book, Library, Librarian
+from .models import Book, Library, Librarian, Author 
 from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
 from django.contrib.auth import login  
 from django.contrib.auth.forms import UserCreationForm  
-
 
 @permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
@@ -43,16 +42,15 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'  # required by the checker
     context_object_name = 'library'
-    
-    # views.py
-def register(request):
-    from django.contrib.auth.forms import UserCreationForm
-    from django.contrib.auth import login
 
+
+
+def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
